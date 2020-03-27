74180f9ffb471c2320dcc3b53d2954b8af3cf0c2
digitalby <yury@digitalby.me>
2020-03-27 13:55:23 +0300

    Initial commit

---
+++ ots-weather-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ots-weather-task
+//
+//  Created by Digital on 27/03/2020.
+//  Copyright © 2020 digitalby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
